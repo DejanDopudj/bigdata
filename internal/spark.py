3df2b41b04fc0d2e32194094fc487ceef90a3221
DejanDopudj <dejandopudj01@gmail.com>
2024-01-21 12:32:57 +0100

    Add delta lake

--- internal/spark.py
+++ internal/spark.py
@@ -11,17 +11,32 @@ class BatchJob:
         self.spark = SparkSession.builder \
         .appName("NBA") \
         .config("spark.master", "spark://spark-master:7077") \
+        .config(
+            "spark.jars.packages",
+            "io.delta:delta-core_2.12:2.2.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0,org.apache.spark:spark-avro_2.12:3.3.0",
+        ) \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        ) \
+        .config("spark.sql.warehouse.dir", "./spark-warehouse") \
+        .config("hive.metastore.uris", "thrift://hive-metastore:9083") \
+        .config("spark.pyspark.python", "python3") \
+        .enableHiveSupport() \
         .getOrCreate()
+
         
     def run(self):
+        self.spark.sql(f"CREATE SCHEMA IF NOT EXISTS primerBaze2")
         folder_list = [folder for folder in os.listdir('./data')]
         print(folder_list)
         csv_reader = csv.reader("./data/2019-20_pbp.csv")
         i = 0
+        df = self.spark.read.format("csv").option("mode", "PERMISSIVE").load("./data/2019-20_pbp.csv")
         df = self.spark.read.csv("./data/2019-20_pbp.csv", header=True, inferSchema=True)
-        self.write_parquet(df, "./processed/test/data/2019-20_pbp.parquet")
-        read_df = self.read_parquet("./processed/test/data/2019-20_pbp.parquet")
-        read_df.show(truncate=False, vertical=True)
+        df.write.format("delta").option("overwriteSchema", "true").saveAsTable("primerBaze2.test2")
+        # df.show(truncate=False, vertical=True)
         self.transform1()
 
     def write_parquet(self, df, path):
@@ -32,14 +47,13 @@ class BatchJob:
         return df
 
     def transform1(self):
-        df = self.read_parquet("./processed/test/data/2019-20_pbp.parquet")
+        df: DataFrame = self.spark.read.format("delta").table("primerBaze2.test2")
         res = (
             df.filter(col("Rebounder").isNotNull())
             .groupBy("Rebounder", "ReboundType")
             .count()
         )
         res.show()
-        self.write_parquet(res, "./processed/test/data/2019-20_rebounds.parquet")
 
 job = BatchJob()
 job.run()
